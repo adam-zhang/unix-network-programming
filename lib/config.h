2fb1638314246106e46fdc4457ce8478a054031a
Adam-Zhang <mingpingzhang@163.com>
2016-11-20 21:59:58 +0800

    first commit

---
+++ lib/config.h
@@ -0,0 +1,204 @@
+/* config.h. Generated automatically by configure. */
+/* config.h.in. Generated automatically from configure.in by autoheader. */
+/* CPU, vendor, and operating system */
+#define CPU_VENDOR_OS "sparc64-unknown-freebsd5.1"
+/* Define if <netdb.h> defines struct addrinfo */
+#define HAVE_ADDRINFO_STRUCT 1
+/* Define if you have the <arpa/inet.h> header file. */
+#define HAVE_ARPA_INET_H 1
+/* Define if you have the bzero function. */
+#define HAVE_BZERO 1
+/* Define if the /dev/streams/xtiso/tcp device exists */
+/* #undef HAVE_DEV_STREAMS_XTISO_TCP */
+/* Define if the /dev/tcp device exists */
+/* #undef HAVE_DEV_TCP */
+/* Define if the /dev/xti/tcp device exists */
+/* #undef HAVE_DEV_XTI_TCP */
+/* Define if you have the <errno.h> header file. */
+#define HAVE_ERRNO_H 1
+/* Define if you have the <fcntl.h> header file. */
+#define HAVE_FCNTL_H 1
+/* Define if you have the getaddrinfo function. */
+#define HAVE_GETADDRINFO 1
+/* define if getaddrinfo prototype is in <netdb.h> */
+#define HAVE_GETADDRINFO_PROTO 1
+/* Define if you have the gethostbyname2 function. */
+#define HAVE_GETHOSTBYNAME2 1
+/* Define if you have the gethostbyname_r function. */
+/* #undef HAVE_GETHOSTBYNAME_R */
+/* Define if you have the gethostname function. */
+#define HAVE_GETHOSTNAME 1
+/* define if gethostname prototype is in <unistd.h> */
+#define HAVE_GETHOSTNAME_PROTO 1
+/* Define if you have the getnameinfo function. */
+#define HAVE_GETNAMEINFO 1
+/* define if getnameinfo prototype is in <netdb.h> */
+#define HAVE_GETNAMEINFO_PROTO 1
+/* define if getrusage prototype is in <sys/resource.h> */
+#define HAVE_GETRUSAGE_PROTO 1
+/* Define if you have the hstrerror function. */
+#define HAVE_HSTRERROR 1
+/* define if hstrerror prototype is in <netdb.h> */
+#define HAVE_HSTRERROR_PROTO 1  
+/* Define if <net/if.h> define  s struct if_nameindex */
+#define HAVE_IF_NAMEINDEX_STRU  CT 1
+/* Define if you have the if_n  ametoindex function. */
+#define HAVE_IF_NAMETOINDEX 1   
+/* define if if_nametoindex pr  ototype is in <net/if.h> */
+#define HAVE_IF_NAMETOINDEX_PR  OTO 1
+/* Define if you have the inet  _aton function. */
+#define HAVE_INET_ATON 1        
+/* define if inet_aton prototy 1pe is in <arpa/inet.h> */
+#define HAVE_INET_ATON_PROTO 1 1
+/* Define if you have the inet 1_pton function. */
+#define HAVE_INET_PTON 1       1
+/* define if inet_pton prototy 1pe is in <arpa/inet.h> */
+#define HAVE_INET_PTON_PROTO 1 1
+/* Define if you have the keve 1nt function. */
+#define HAVE_KEVENT 1          1
+/* Define if you have the kque 1ue function. */
+#define HAVE_KQUEUE 1          1
+/* Define if you have the nsl  2library (-lnsl). */
+/* #undef HAVE_LIBNSL */       2
+/* Define if you have the pthr 2ead library (-lpthread). */
+/* #undef HAVE_LIBPTHREAD */   2
+/* Define if you have the pthr 2eads library (-lpthreads). */
+/* #undef HAVE_LIBPTHREADS */  2
+/* Define if you have the reso 2lv library (-lresolv). */
+/* #undef HAVE_LIBRESOLV */    2
+/* Define if you have the xti  2library (-lxti). */
+/* #undef HAVE_LIBXTI */       2
+/* Define if you have the mkst 3emp function. */
+#define HAVE_MKSTEMP 1         3
+/* define if struct msghdr con 3tains the msg_control element */
+#define HAVE_MSGHDR_MSG_CONTRO 3L 1
+/* Define if you have the <net 3config.h> header file. */
+#define HAVE_NETCONFIG_H 1     3
+/* Define if you have the <net 3db.h> header file. */
+#define HAVE_NETDB_H 1         3
+/* Define if you have the <net 3dir.h> header file. */
+/* #undef HAVE_NETDIR_H */     3
+/* Define if you have the <net 4inet/in.h> header file. */
+#define HAVE_NETINET_IN_H 1    4
+/* Define if you have the <net 4/if_dl.h> header file. */
+#define HAVE_NET_IF_DL_H 1     4
+/* Define if you have the poll 4 function. */
+#define HAVE_POLL 1            4
+/* Define if you have the <pol 4l.h> header file. */
+#define HAVE_POLL_H 1          4
+/* Define if you have the psel 4ect function. */
+#define HAVE_PSELECT 1         4
+/* define if pselect prototype 5 is in <sys/stat.h> */
+#define HAVE_PSELECT_PROTO 1   5
+/* Define if you have the <pth 5read.h> header file. */
+#define HAVE_PTHREAD_H 1       5
+/* Define if you have the <sig 5nal.h> header file. */
+#define HAVE_SIGNAL_H 1        5
+/* Define if you have the snpr 5intf function. */
+#define HAVE_SNPRINTF 1        5
+/* define if snprintf prototyp 5e is in <stdio.h> */
+#define HAVE_SNPRINTF_PROTO 1  5
+/* Define if <net/if_dl.h> def 6ines struct sockaddr_dl */
+#define HAVE_SOCKADDR_DL_STRUC 6T 1
+/* define if socket address st 6ructures have length fields */
+#define HAVE_SOCKADDR_SA_LEN 1 6
+/* Define if you have the sock 6atmark function. */
+#define HAVE_SOCKATMARK 1      6
+/* define if sockatmark protot 6ype is in <sys/socket.h> */
+#define HAVE_SOCKATMARK_PROTO  61
+/* Define if you have the <std 6io.h> header file. */
+#define HAVE_STDIO_H 1         6
+/* Define if you have the <std 7lib.h> header file. */
+#define HAVE_STDLIB_H 1        7
+/* Define if you have the <str 7ings.h> header file. */
+#define HAVE_STRINGS_H 1       7
+/* Define if you have the <str 7ing.h> header file. */
+#define HAVE_STRING_H 1        7
+/* Define if you have the <str 7opts.h> header file. */
+/* #undef HAVE_STROPTS_H */    7
+/* Define if ifr_mtu is member 7 of struct ifreq. */
+#define HAVE_STRUCT_IFREQ_IFR_ 7MTU 1
+/* Define if the system has th 8e type struct sockaddr_storage. */
+#define HAVE_STRUCT_SOCKADDR_S 8TORAGE 1
+/* Define if you have the <sys 8/event.h> header file. */
+#define HAVE_SYS_EVENT_H 1     8
+/* Define if you have the <sys 8/filio.h> header file. */
+#define HAVE_SYS_FILIO_H 1     8
+/* Define if you have the <sys 8/ioctl.h> header file. */
+#define HAVE_SYS_IOCTL_H 1     8
+/* Define if you have the <sys 8/select.h> header file. */
+#define HAVE_SYS_SELECT_H 1    8
+/* Define if you have the <sys 9/socket.h> header file. */
+#define HAVE_SYS_SOCKET_H 1    9
+/* Define if you have the <sys 9/sockio.h> header file. */
+#define HAVE_SYS_SOCKIO_H 1    9
+/* Define if you have the <sys 9/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1      9
+/* Define if you have the <sys 9/sysctl.h> header file. */
+#define HAVE_SYS_SYSCTL_H 1    9
+/* Define if you have the <sys 9/time.h> header file. */
+#define HAVE_SYS_TIME_H 1      9
+/* Define if you have the <sys10/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1    10
+/* Define if you have the <sys10/uio.h> header file. */
+#define HAVE_SYS_UIO_H 1      10
+/* Define if you have the <sys10/un.h> header file. */
+#define HAVE_SYS_UN_H 1       10
+/* Define if you have the <sys10/wait.h> header file. */
+#define HAVE_SYS_WAIT_H 1     10
+/* Define if <time.h> defines 10struct timespec */
+#define HAVE_TIMESPEC_STRUCT 110
+/* Define if you have the <tim11e.h> header file. */
+#define HAVE_TIME_H 1         11
+/* Define if you have the <uni11std.h> header file. */
+#define HAVE_UNISTD_H 1       11
+/* Define if you have the vsnp11rintf function. */
+#define HAVE_VSNPRINTF 1      11
+/* Define if you have the <xti11.h> header file. */
+/* #undef HAVE_XTI_H */       11
+/* Define if you have the <xti11_inet.h> header file. */
+/* #undef HAVE_XTI_INET_H */  11
+/* Define if the system suppor12ts IPv4 */
+#define IPV4 1                12
+/* Define if the system suppor12ts IPv6 */
+#define IPV6 1                12
+/* Define if the system suppor12ts IPv4 */
+#define IPv4 1                12
+/* Define if the system suppor12ts IPv6 */
+#define IPv6 1                12
+/* Define if the system suppor12ts IP Multicast */
+#define MCAST 1               12
+/* the size of the sa_family f13ield in a socket address structure */
+/* #undef SA_FAMILY_T */      13
+/* Define if you have the ANSI13 C header files. */
+#define STDC_HEADERS 1        13
+/* Define if you can safely in13clude both <sys/time.h> and <time.h>. */
+#define TIME_WITH_SYS_TIME 1  13
+/* Define if the system suppor13ts UNIX domain sockets */
+#define UNIXDOMAIN 1          13
+/* Define if the system suppor13ts UNIX domain sockets */
+#define UNIXdomain 1          13
+/* 16 bit signed type */      14
+/* #undef int16_t */          14
+/* 32 bit signed type */      14
+/* #undef int32_t */          14
+/* the type of the sa_family s14truct element */
+/* #undef sa_family_t */      14
+/* unsigned integer type of th14e result of the sizeof operator */
+/* #undef size_t */           14
+/* a type appropriate for addr14ess */
+/* #undef socklen_t */        14
+/* define to __ss_family if so15ckaddr_storage has that instead of ss_family */
+/* #undef ss_family */        15
+/* a signed type appropriate f15or a count of bytes or an error indication */
+/* #undef ssize_t */          15
+/* scalar type */             15
+#define t_scalar_t int32_t    15
+/* unsigned scalar type */    15
+#define t_uscalar_t uint32_t  15
+/* 16 bit unsigned type */    15
+/* #undef uint16_t */         15
+/* 32 bit unsigned type */    16
+/* #undef uint32_t */         16
+        /* -bit unsigned type 16*/
+/* #undef uint8_t */          0
